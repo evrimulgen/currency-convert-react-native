d5cc992cc5bfc76b646419b099f9d66071fcc007
shulg <verybluebot@gmail.com>
2017-07-30 18:51:46 +0300

    connecting navigation with redux state

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React  from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
+import { addNavigationHelpers } from 'react-navigation';
 
 import Navigator from './config/router';
 import store from './config/store';
@@ -18,9 +19,24 @@ EStyleSheet.build({
     $darkText: '#343434'
 });
 
-export default App = () => (
+const App = ({dispatch, nav}) => (
+    <Navigator
+        navigation={addNavigationHelpers({
+            dispatch,
+            state: nav
+        })}
+    />
+);
+
+const mapStateToProps = (state) => ({
+    nav: state.nav
+});
+
+const AppWithNavigation = connect(mapStateToProps)(App);
+
+export default () => (
     <Provider store={store}>
-        <Navigator onNavigationStateChange={null} />
+        <AppWithNavigation />
     </Provider>
 )
 
